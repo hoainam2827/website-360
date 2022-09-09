f2eaac040119ba69b753c4afca7b667447b30fc8
hoainam2827 <Hoainam2827@gmail.com>
2022-09-09 11:19:34 +0700

    init web 360

---
+++ data.js
@@ -0,0 +1,307 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-nasa1",
+      "name": "nasa1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "yaw": 0.2987712103146354,
+        "pitch": 0.002766574818963363,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0857644768132726,
+          "pitch": 0.11016931443839795,
+          "rotation": 6.283185307179586,
+          "target": "1-nasa2"
+        },
+        {
+          "yaw": 0.17905061873534045,
+          "pitch": 0.16988613826663013,
+          "rotation": 0,
+          "target": "2-nasa3"
+        },
+        {
+          "yaw": 0.2057624674962515,
+          "pitch": 0.04915193214111646,
+          "rotation": 0,
+          "target": "3-nasa4"
+        },
+        {
+          "yaw": -2.94965386018918,
+          "pitch": 0.05986996470233663,
+          "rotation": 0,
+          "target": "6-nasa7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-nasa2",
+      "name": "nasa2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0410479226961744,
+          "pitch": 0.13519284083891847,
+          "rotation": 0,
+          "target": "4-nasa5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-nasa3",
+      "name": "nasa3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1012933720827505,
+          "pitch": 0.16956077102344125,
+          "rotation": 0,
+          "target": "0-nasa1"
+        },
+        {
+          "yaw": -2.6821542324548773,
+          "pitch": 0.30543583388731044,
+          "rotation": 0.7853981633974483,
+          "target": "1-nasa2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-nasa4",
+      "name": "nasa4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.107412329752554,
+          "pitch": 0.06734079293788753,
+          "rotation": 0,
+          "target": "0-nasa1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-nasa5",
+      "name": "nasa5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6778506135715201,
+          "pitch": 0.1764488808906357,
+          "rotation": 10.995574287564278,
+          "target": "1-nasa2"
+        },
+        {
+          "yaw": 0.006661637862539038,
+          "pitch": 0.08638513126183867,
+          "rotation": 0,
+          "target": "5-nasa6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-nasa6",
+      "name": "nasa6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3878802431048154,
+          "pitch": 0.12859860557314207,
+          "rotation": 0,
+          "target": "4-nasa5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-nasa7",
+      "name": "nasa7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3530716816302597,
+          "pitch": 0.10334305167568303,
+          "rotation": 0,
+          "target": "0-nasa1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
